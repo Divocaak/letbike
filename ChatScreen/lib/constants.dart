ab306c83cba29cf99e6998745fa5d3f573b890f1
LukasTomanek <78879757+LukasTomanek@users.noreply.github.com>
2021-03-15 08:37:38 +0100

    Add files via upload

---
+++ ChatScreen/lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Colors.green;
+const kSecondaryColor = Color(0xFFFE9901);
+const kContentColorLightTheme = Color(0x2F7B7B75);
+const kContentColorDarkTheme = Color(0xFFF5FCF9);
+const kWarninngColor = Color(0xFFF3BB1C);
+const kErrorColor = Colors.redAccent;
+
+const kDefaultPadding = 20.0;
