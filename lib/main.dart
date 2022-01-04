3f6ff8f3cdd5c8d10d997bb1d250bf90717391b8
Vojtěch Divoký <divokyvojta@gmail.com>
2022-01-04 21:05:58 +0100

    native => responsive banner ads, works

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:letbike/firstScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(LetBike());
 }
