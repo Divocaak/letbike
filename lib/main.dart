60fb665ffecf1a3276d1259f855287e2e9869d76
Vojtěch Divoký <divokyvojta@gmail.com>
2022-04-06 23:05:12 +0200

    orientation secured

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:letbike/screens/first_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'general/firebase_options.dart';
@@ -6,7 +7,8 @@ import 'general/firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(LetBike());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) => runApp(LetBike()));
 }
 
 class LetBike extends StatelessWidget {
