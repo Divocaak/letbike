5ad09ae106d76d15cfbef5475875e95a51c70642
Vojtěch divoký <vojtech.divoky@polyweb.cz>
2022-09-13 14:21:22 +0200

    article page and list update #90

--- lib/screens/articles_screen.dart
+++ lib/screens/articles_screen.dart
@@ -1,48 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:letbike/objects/article.dart';
 import 'package:letbike/widgets/new/button_main.dart';
-import 'package:letbike/widgets/error_widgets.dart';
-import 'package:letbike/general/settings.dart';
 import 'package:letbike/remote/articles.dart';
+import 'package:letbike/widgets/new/future_card_list.dart';
 import 'package:letbike/widgets/new/page_body.dart';
 
-class ArticlesScreen extends StatefulWidget {
-  @override
-  _ArticlesScreenState createState() => _ArticlesScreenState();
-}
+class ArticlesScreen extends StatelessWidget {
+  ArticlesScreen({Key? key}) : super(key: key);
 
-class _ArticlesScreenState extends State<ArticlesScreen> {
   @override
   Widget build(BuildContext context) => PageBody(
-      body: RefreshIndicator(
-          onRefresh: _pullRefresh,
-          color: kPrimaryColor,
-          strokeWidth: 5,
-          child: SizedBox.expand(
-              child: FutureBuilder<List<Article>?>(
-                  future: RemoteArticles.getAllArticles(),
-                  builder: (context, snapshot) {
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                        return Center(child: Image.asset("assets/load.gif"));
-                      default:
-                        if (snapshot.hasError)
-                          return ErrorWidgets.futureBuilderError();
-                        else if (!snapshot.hasData ||
-                            (snapshot.hasData && snapshot.data!.length < 1))
-                          return ErrorWidgets.futureBuilderEmpty();
-                        return ListView.builder(
-                            itemCount: snapshot.data!.length,
-                            itemBuilder: (context, i) =>
-                                snapshot.data![i].buildCard(context));
-                    }
-                  }))),
+      body: FutureCardList(
+          buildFunction: (object) => (object as Article).buildCard(context),
+          fetchFunction: () => RemoteArticles.getAllArticles()),
       mainButton: MainButton(
           iconData: Icons.arrow_back,
           onPressed: () => Navigator.of(context).pop()));
-
-  Future<void> _pullRefresh() async {
-    // TODO mby todo?
-    widget.createState();
-  }
 }
